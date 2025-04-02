b30e02095f1e583cbf0e173b3568343514629d63
luckac69 <luckac69@gmail.com>
2025-04-02 19:26:40 -0400

    well the names aren't in the *right* place, but its a temp thing, I gota move on

--- Scenes/Main.gd
+++ Scenes/Main.gd
@@ -23,84 +23,6 @@ func load_armies():
 
 
 ##########LOADING REGIONS##################
-func _getPolygonCenter(polygons: Array) -> Vector2:
-	var center : Vector2 = Vector2.ZERO
-	var numPoints : int = 0
-	
-	for polygon in polygons:
-		for vertext in polygon:
-			center += vertext
-			numPoints += 1 
-
-	return center / numPoints
-
-## Function to check if a point is inside a polygon
-#func is_point_in_polygon(point: Vector2, polygon: Array) -> bool:
-#	var inside = false
-#	var n = polygon.size()
-#	for i in range(n):
-#		var j = (i + 1) % n
-#		if ((polygon[i].y > point.y) != (polygon[j].y > point.y)) and (point.x < (polygon[j].x - polygon[i].x) * (point.y - polygon[i].y) / (polygon[j].y - polygon[i].y) + polygon[i].x):
-#			inside = !inside
-#	return inside
-#
-## Function to calculate the distance to the edges of the polygon
-#func distance_to_polygon_edges(point: Vector2, polygon: Array) -> float:
-#	var min_distance = INF
-#	for i in range(polygon.size()):
-#		var j = (i + 1) % polygon.size()
-#		var edge_start = polygon[i]
-#		var edge_end = polygon[j]
-#		var edge_vector = edge_end - edge_start
-#		var edge_length = edge_vector.length()
-#		
-#		if edge_length == 0:
-#			continue
-#		
-#		var t = clamp(((point - edge_start).dot(edge_vector)) / (edge_length * edge_length), 0, 1)
-#		var closest_point = edge_start + edge_vector * t
-#		var distance = point.distance_to(closest_point)
-#		
-#		min_distance = min(min_distance, distance)
-#	return min_distance
-#
-## Function to find the pole of inaccessibility for a list of polygon objects
-#func find_poles_of_inaccessibility(polygons: Array, sample_count: int = 1000) -> Array:
-#	var poles = []
-#	
-#	for polygon in polygons:
-#		# Assuming polygon has a method or property to get its points
-#		var polygon_points = polygon.get_points()  # Adjust this line based on your polygon object
-#		
-#		var best_point = Vector2()
-#		var max_min_distance = -1.0
-#		
-#		# Calculate bounding box for sampling
-#		var min_x = polygon_points[0].x
-#		var max_x = polygon_points[0].x
-#		var min_y = polygon_points[0].y
-#		var max_y = polygon_points[0].y
-#		
-#		for point in polygon_points:
-#			min_x = min(min_x, point.x)
-#			max_x = max(max_x, point.x)
-#			min_y = min(min_y, point.y)
-#			max_y = max(max_y, point.y)
-#		
-#		for i in range(sample_count):
-#			var sample_point = Vector2(randf_range(min_x, max_x), randf_range(min_y, max_y))
-#			if is_point_in_polygon(sample_point, polygon_points):
-#				var distance = distance_to_polygon_edges(sample_point, polygon_points)
-#				if distance > max_min_distance:
-#					max_min_distance = distance
-#					best_point = sample_point
-#		
-#		poles.append(best_point)
-#	
-#	return poles
-
-
-#####################################3
 
 func load_regions():
 	var image = mapImage.get_texture().get_image()
@@ -147,6 +69,24 @@ func load_regions():
 			region.add_child(region_line)
 
 			
+#######GETTING THE CENTER OF THE POLYGON#################
+func _getPolygonCenter(polygons: Array) -> Vector2:
+	var center : Vector2 = Vector2.ZERO
+	var numPoints : int = 0
+	
+	for polygon in polygons:
+		for vertext in polygon:
+			center += vertext
+			numPoints += 1 
+
+	return center / numPoints
+
+
+
+func _calcBoundingBox(polygons: Array) -> Vector4:
+	var boundingBox : Vector4
+
+	return boundingBox
 
 
 
